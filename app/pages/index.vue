fae856cd48906b09722dd6a35ea7e70d5223f853
hiuuuu <nguyenminhhieu.31018@gmail.com>
2026-03-14 18:26:59 +0700

    feat:

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,10 +3,73 @@
     <div class="grid grid-cols-5 grid-rows-[130px_130px_250px] gap-5">
       <div class="col-span-1 row-span-1">
         <UiBaseStatCard
-          title="Total Users"
-          value="1,234"
-          unit="users"
-          :trend="125"
+          title="Total's Orders"
+          value="245"
+          unit="units"
+          :trend="12.5"
+          unit-trend="%"
+          trend-label="vs yesterday"
+          :is-loading="!mounted"
+        >
+          <template #icon>
+            <UBadge color="success" variant="soft">
+              <UIcon
+                name="material-symbols:orders-outline-rounded"
+                class="w-5 h-5 text-green-600"
+              />
+            </UBadge>
+          </template>
+        </UiBaseStatCard>
+      </div>
+      <div class="col-span-1 row-span-1">
+        <UiBaseStatCard
+          title="Daily Revenue"
+          value="$48.5"
+          unit="K"
+          :trend="0.3"
+          unit-trend="%"
+          trend-label="vs yesterday"
+          :is-loading="!mounted"
+        >
+          <template #icon>
+            <UBadge color="warning" variant="soft">
+              <UIcon
+                name="i-heroicons-chart-bar"
+                class="w-5 h-5 text-yellow-600"
+              />
+            </UBadge>
+          </template>
+        </UiBaseStatCard>
+      </div>
+      <div class="col-span-1 row-span-1">
+        <UiBaseStatCard
+          title="On-time Rate"
+          value="92.2"
+          unit="%"
+          :trend="5.7"
+          unit-trend="%"
+          trend-label="vs yesterday"
+          :is-loading="!mounted"
+        >
+          <template #icon>
+            <UBadge color="success" variant="soft">
+              <UIcon
+                name="ic:twotone-watch-later"
+                class="w-5 h-5 text-green-600"
+              />
+            </UBadge>
+          </template>
+        </UiBaseStatCard>
+      </div>
+      <div class="col-span-2 row-span-2">
+        <UiBusinessPerformanceWidgetComponent :is-loading="!mounted" />
+      </div>
+      <div class="col-span-1 row-span-1">
+        <UiBaseStatCard
+          title="Production Output"
+          value="1,235"
+          unit="units"
+          :trend="-3.2"
           unit-trend="%"
           trend-label="vs yesterday"
           :is-loading="!mounted"
@@ -21,32 +84,32 @@
           </template>
         </UiBaseStatCard>
       </div>
-      <div class="">
+      <div class="col-span-1 row-span-1">
         <UiBaseStatCard
-          title="Total Users"
-          value="1,234"
-          unit="users"
-          :trend="-15"
+          title="Delayed Orders"
+          value="18"
+          unit="orders"
+          :trend="-3.2"
           unit-trend="%"
           trend-label="vs yesterday"
           :is-loading="!mounted"
         >
           <template #icon>
-            <UBadge color="warning" variant="soft">
+            <UBadge color="error" variant="soft">
               <UIcon
-                name="i-heroicons-chart-bar"
-                class="w-5 h-5 text-yellow-600"
+                name="tabler:truck-delivery"
+                class="w-5 h-5 text-red-500"
               />
             </UBadge>
           </template>
         </UiBaseStatCard>
       </div>
-      <div class="">
+      <div class="col-span-1 row-span-1">
         <UiBaseStatCard
-          title="Total Users"
-          value="1,234"
-          unit="users"
-          :trend="-15"
+          title="Defect Rate"
+          value="5.5"
+          unit="%"
+          :trend="-15.5"
           unit-trend="%"
           trend-label="vs yesterday"
           :is-loading="!mounted"
@@ -54,70 +117,7 @@
           <template #icon>
             <UBadge color="warning" variant="soft">
               <UIcon
-                name="i-heroicons-chart-bar"
-                class="w-5 h-5 text-yellow-600"
-              />
-            </UBadge>
-          </template>
-        </UiBaseStatCard>
-      </div>
-      <div class="col-span-2 row-span-2">
-        <UiBusinessPerformanceWidgetComponent :is-loading="!mounted" />
-      </div>
-      <div class="">
-        <UiBaseStatCard
-          title="Total Users"
-          value="1,234"
-          unit="users"
-          :trend="-15"
-          unit-trend="%"
-          trend-label="vs yesterday"
-          :is-loading="!mounted"
-        >
-          <template #icon>
-            <UBadge color="warning" variant="soft">
-              <UIcon
-                name="i-heroicons-chart-bar"
-                class="w-5 h-5 text-yellow-600"
-              />
-            </UBadge>
-          </template>
-        </UiBaseStatCard>
-      </div>
-      <div class="">
-        <UiBaseStatCard
-          title="Total Users"
-          value="1,234"
-          unit="users"
-          :trend="-15"
-          unit-trend="%"
-          trend-label="vs yesterday"
-          :is-loading="!mounted"
-        >
-          <template #icon>
-            <UBadge color="warning" variant="soft">
-              <UIcon
-                name="i-heroicons-chart-bar"
-                class="w-5 h-5 text-yellow-600"
-              />
-            </UBadge>
-          </template>
-        </UiBaseStatCard>
-      </div>
-      <div class="">
-        <UiBaseStatCard
-          title="Total Users"
-          value="1,234"
-          unit="users"
-          :trend="-15"
-          unit-trend="%"
-          trend-label="vs yesterday"
-          :is-loading="!mounted"
-        >
-          <template #icon>
-            <UBadge color="warning" variant="soft">
-              <UIcon
-                name="i-heroicons-chart-bar"
+                name="fluent:line-horizontal-5-error-20-filled"
                 class="w-5 h-5 text-yellow-600"
               />
             </UBadge>
